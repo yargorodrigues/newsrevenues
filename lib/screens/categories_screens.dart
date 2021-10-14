d9f79795625896d011c9fd5e3ff2ae11998f7079
yargorodrigues <marrocos07@gmail.com>
2021-10-14 10:54:46 -0300

    Versao 1

---
+++ lib/screens/categories_screens.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:newsrevenues/components/category_item.dart';
+import 'package:newsrevenues/data/dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+        padding: const EdgeInsets.all(15),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: dummyCategories.map((cat) {
+          return CategoryItem(cat);
+        }).toList(),
+      );
+  }
+}
